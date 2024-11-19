99a3b815742616859b5d83a5471cc8cf7c418844
MatheusPelissari <m.pelissari@outlook.com>
2024-11-18 23:17:06 -0300

    portfolio-arquivos

---
+++ portifolio.js
@@ -0,0 +1,58 @@
+
+function showPopup() {
+document.getElementById('popup').style.display = 'block';
+document.getElementById('overlay').style.display = 'block';
+}
+
+function hidePopup() {
+document.getElementById('popup').style.display = 'none';
+document.getElementById('overlay').style.display = 'none';
+}
+
+function showPopup2() {
+document.getElementById('popup2').style.display = 'block';
+document.getElementById('overlay').style.display = 'block';
+}
+
+function hidePopup2() {
+document.getElementById('popup2').style.display = 'none';
+document.getElementById('overlay').style.display = 'none';
+}
+
+function showPopup3() {
+document.getElementById('popup3').style.display = 'block';
+document.getElementById('overlay').style.display = 'block';
+}
+
+function hidePopup3() {
+document.getElementById('popup3').style.display = 'none';
+document.getElementById('overlay').style.display = 'none';
+}
+
+function showPopup4() {
+document.getElementById('popup4').style.display = 'block';
+document.getElementById('overlay').style.display = 'block';
+}
+
+function hidePopup4() {
+document.getElementById('popup4').style.display = 'none';
+document.getElementById('overlay').style.display = 'none';
+}
+function showPopup5() {
+document.getElementById('popup5').style.display = 'block';
+document.getElementById('overlay').style.display = 'block';
+}
+
+function hidePopup5() {
+document.getElementById('popup5').style.display = 'none';
+document.getElementById('overlay').style.display = 'none';
+}
+function showPopup6() {
+document.getElementById('popup6').style.display = 'block';
+document.getElementById('overlay').style.display = 'block';
+}
+
+function hidePopup6() {
+document.getElementById('popup6').style.display = 'none';
+document.getElementById('overlay').style.display = 'none';
+}
